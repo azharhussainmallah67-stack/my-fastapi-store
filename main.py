db5b3e4fac5068db49d9fe83a93f34dd9b9a381a
azharhussainmallah67-stack <azharhussainmallah67@gmail.com>
2026-04-28 18:29:37 +0500

    Use environment variable for uvicorn port
    Update uvicorn run command to use environment variable for port.

--- main.py
+++ main.py
@@ -206,4 +206,7 @@ async def logout():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    import os
+    # Render khud port batayega, agar nahi to default 8000 use hoga
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
